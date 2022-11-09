ce0a39ed0d6a0180123198ee9c6566ecae4f995a
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-11-09 09:38:58 -0300

    feat: Virtual Component added

---
+++ src/components/virtual/Virtual.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactCompareImage from 'react-compare-image';
+
+import Before from '../../assets/before.png';
+import After from '../../assets/after.png';
+import Shade from '../../assets/shade.png';
+import css from './Virtual.module.css';
+
+const Virtual = () => {
+  return (
+    <div className={css.Virtual}>
+      <div className={css.left}>
+        <span>Virtual Try-On</span>
+        <span>Never Buy the wrong Shade Again!</span>
+        <span>Try Now!</span>
+        <img src={Shade} alt='shade' />
+      </div>
+      <div className={css.right}>
+        <div className={css.wrapper}>
+          <ReactCompareImage leftImage={Before} rightImage={After} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Virtual;
