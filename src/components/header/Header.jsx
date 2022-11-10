36a9ffcc6d9b826014c320aafd46d69a3918325a
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-11-10 15:35:05 -0300

    feat: Tablet view added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CgShoppingBag } from 'react-icons/cg';
+import { GoThreeBars } from 'react-icons/go';
 
 import Logo from '../../assets/logo.png';
 import css from './Header.module.css';
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(true);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div className={css.container}>
       <div className={css.logo}>
         <img src={Logo} alt='Amazon' /> <span>amazon</span>
       </div>
       <div className={css.right}>
-        <div className={css.menu}>
-          <ul className={css.menu}>
-            <li>Collections</li>
-            <li>Brands</li>
-            <li>New</li>
-            <li>Sales</li>
-            <li>ENG</li>
-          </ul>
+        <div className={css.bars} onClick={toggleMenu}>
+          <GoThreeBars />
         </div>
+        <ul
+          className={css.menu}
+          style={{ display: showMenu ? 'inherit' : 'none' }}>
+          <li>Collections</li>
+          <li>Brands</li>
+          <li>New</li>
+          <li>Sales</li>
+          <li>ENG</li>
+        </ul>
         <input type='text' className={css.search} placeholder='Search...' />
         <CgShoppingBag className={css.cart} />
       </div>
